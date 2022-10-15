3e6da607581197f7e316fde07dfe5d8b69e244d4
Abhishek Patel <patelabhishek2007@gmail.com>
2022-10-15 16:53:03 +0530

    added snippets/CustomDialog

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Contains Java/xml Android Boilerplate codes, utils, activities to speed up development process
 
 
+# Steps to use:
+
+1. Add boilerplate package inside Source com.domain.appname package.
+2. Add required dependecies in module level build.gradle and sync it.
+3. Add res and layout files in their respective directories.
+4. Use and Customize these classes and remove unused classes / activities.
+
--- boilerplate/data/firebase/FirebaseAuthRepository.java
+++ boilerplate/data/firebase/FirebaseAuthRepository.java
@@ -1,11 +1,11 @@
 package com.ciberciti.subscraze.boilerplate.data.firebase;
 
 import android.app.Application;
-import android.net.Uri;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.MutableLiveData;
+import com.ciberciti.subscraze.boilerplate.data.model.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -32,42 +32,55 @@ public class FirebaseAuthRepository {
         }
     }
 
-    public void register(String email, String password) {
+    public MutableLiveData<Boolean> register(String displayName, String email, String password) {
+        MutableLiveData<Boolean> loginLiveData = new MutableLiveData<>();
         firebaseAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(ContextCompat.getMainExecutor(application), new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             userLiveData.postValue(firebaseAuth.getCurrentUser());
-                            saveUserData();
+                            updateDisplayName(displayName);
+                            loginLiveData.postValue(true);
+                            saveUserData(new User(firebaseAuth.getUid(), firebaseAuth.getCurrentUser().getDisplayName(), getCurrentUser().getEmail()));
+                            loggedOutLiveData.postValue(false);
                         } else {
-                            Toast.makeText(application.getApplicationContext(), "Registration Failure: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            loginLiveData.postValue(false);
+                            Toast.makeText(application.getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
+        return loginLiveData;
     }
 
     //TODO: Implement saveUserData
-    public void saveUserData() {
+    public void saveUserData(User user) {
 
     }
 
-    public void login(String email, String password) {
+    public MutableLiveData<Boolean> login(String email, String password) {
+        MutableLiveData<Boolean> loginLiveData = new MutableLiveData<>();
         firebaseAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(ContextCompat.getMainExecutor(application), new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            userLiveData.postValue(firebaseAuth.getCurrentUser());
+                            userLiveData.postValue(task.getResult().getUser());
+                            loginLiveData.postValue(true);
+                            loggedOutLiveData.postValue(false);
                         } else {
-                            Toast.makeText(application.getApplicationContext(), "Login Failure: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            loginLiveData.postValue(false);
+                            Toast.makeText(application.getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
+        return loginLiveData;
     }
 
     public void logOut() {
         firebaseAuth.signOut();
+        Toast.makeText(application, "Logged Out:" + (firebaseAuth.getCurrentUser() == null), Toast.LENGTH_SHORT).show();
+        userLiveData.postValue(firebaseAuth.getCurrentUser());
         loggedOutLiveData.postValue(true);
     }
 
@@ -83,10 +96,10 @@ public class FirebaseAuthRepository {
         return firebaseAuth.getCurrentUser();
     }
 
-    public void updateDisplayName(String newDisplayName) {
+    public MutableLiveData<Boolean> updateDisplayName(String newDisplayName) {
+        MutableLiveData<Boolean> isSuccessful = new MutableLiveData<>();
         UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                .setDisplayName("Jane Q. User")
-                .setPhotoUri(Uri.parse("https://example.com/jane-q-user/profile.jpg"))
+                .setDisplayName(newDisplayName)
                 .build();
 
         firebaseAuth.getCurrentUser().updateProfile(profileUpdates)
@@ -95,83 +108,107 @@ public class FirebaseAuthRepository {
                     public void onComplete(@NonNull Task<Void> task) {
 
                         if (task.isSuccessful()) {
+                            isSuccessful.setValue(true);
                             userLiveData.postValue(firebaseAuth.getCurrentUser());
                         } else {
-                            Toast.makeText(application.getApplicationContext(), "UpdateFailed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            isSuccessful.setValue(false);
+                            Toast.makeText(application.getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     }
 
                 });
+        return isSuccessful;
     }
 
-    public void updateEmail(String newEmail) {
-        firebaseAuth.getCurrentUser().updateEmail("user@example.com")
+    public MutableLiveData<Boolean> updateEmail(String newEmail) {
+        MutableLiveData<Boolean> isSuccessful = new MutableLiveData<>();
+        firebaseAuth.getCurrentUser().updateEmail(newEmail)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
+                            isSuccessful.setValue(true);
                             userLiveData.postValue(firebaseAuth.getCurrentUser());
                         } else {
-                            Toast.makeText(application.getApplicationContext(), "UpdateFailed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            isSuccessful.setValue(false);
+                            Toast.makeText(application.getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
+        return isSuccessful;
     }
 
-    public void sendVerificationEmail() {
+    public MutableLiveData<Boolean> sendVerificationEmail() {
+        MutableLiveData<Boolean> isSuccessful = new MutableLiveData<>();
         firebaseAuth.getCurrentUser().sendEmailVerification()
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
+                            isSuccessful.setValue(true);
                             userLiveData.postValue(firebaseAuth.getCurrentUser());
                         } else {
-                            Toast.makeText(application.getApplicationContext(), "UpdateFailed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            isSuccessful.setValue(false);
+                            Toast.makeText(application.getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
+        return isSuccessful;
+        
     }
 
-    public void sendPasswordResetEmail() {
-        firebaseAuth.sendPasswordResetEmail(getCurrentUser().getEmail())
+    public MutableLiveData<Boolean> sendPasswordResetEmail(String email) {
+        MutableLiveData<Boolean> isSuccessful = new MutableLiveData<>();
+        firebaseAuth.sendPasswordResetEmail(email)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
+                            isSuccessful.setValue(true);
                             userLiveData.postValue(firebaseAuth.getCurrentUser());
                         } else {
-                            Toast.makeText(application.getApplicationContext(), "UpdateFailed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            isSuccessful.setValue(false);
+                            Toast.makeText(application.getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
+        return isSuccessful;
     }
 
-    public void updatePassword(String newPassword) {
+    public MutableLiveData<Boolean> updatePassword(String newPassword) {
+        MutableLiveData<Boolean> isSuccessful = new MutableLiveData<>();
         firebaseAuth.getCurrentUser().updatePassword(newPassword)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
+                            isSuccessful.setValue(true);
                             userLiveData.postValue(firebaseAuth.getCurrentUser());
                         } else {
-                            Toast.makeText(application.getApplicationContext(), "UpdateFailed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            isSuccessful.setValue(false);
+                            Toast.makeText(application.getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
+        return isSuccessful;
     }
 
-    public void deleteAccount() {
+    public MutableLiveData<Boolean> deleteAccount() {
+        MutableLiveData<Boolean> isSuccessful = new MutableLiveData<>();
         firebaseAuth.getCurrentUser().delete()
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
+                            isSuccessful.setValue(true);
                             userLiveData.postValue(firebaseAuth.getCurrentUser());
                         } else {
-                            Toast.makeText(application.getApplicationContext(), "Failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            isSuccessful.setValue(false);
+                            Toast.makeText(application.getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
+        return isSuccessful;
     }
 
 
